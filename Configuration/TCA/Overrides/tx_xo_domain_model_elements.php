0fe2047e325fe9d685ff006d3852a42ceba77c50
Christian Pschorr <c.pschorr@datamints.com>
2022-05-22 21:53:44 +0200

    Transparente Bilder in Werten: Farbliche Änderungen im Listing; Zuschnitt für Detailansicht

--- Configuration/TCA/Overrides/tx_xo_domain_model_elements.php
+++ Configuration/TCA/Overrides/tx_xo_domain_model_elements.php
@@ -27,3 +27,44 @@ $tmpKistValuesElementsColumns = [
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_xo_domain_model_elements', $tmpKistValuesElementsColumns);
+
+$GLOBALS['TCA']['tx_xo_domain_model_elements']['types']['ce_kist_values_default']['columnsOverrides']['media'] = [
+	'l10n_mode' => 'exclude',
+	'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+		'media',
+		[
+			'overrideChildTca' => [
+				'columns' => [
+					'crop' => [
+						'config' => [
+							'cropVariants' => [
+								'thumbnail' => [
+									'title' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_crop_variant.thumbnail',
+									'allowedAspectRatios' => [
+										'NaN' => [
+											'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.free',
+											'value' => 0.0
+										],
+									],
+									'selectedRatio' => 'NaN',
+								],
+								'fullsize' => [
+									'title' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_crop_variant.fullsize',
+									'allowedAspectRatios' => [
+										'NaN' => [
+											'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.free',
+											'value' => 0.0
+										],
+									],
+									'selectedRatio' => 'NaN',
+								],
+							]
+						]
+					]
+				]
+			],
+			'maxitems' => 1
+		],
+		$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
+	),
+];
